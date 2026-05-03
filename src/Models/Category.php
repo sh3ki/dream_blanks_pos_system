52f03f8142e34884843fe562fb528063f12d51a8
sh3ki <shekaigarcia@gmail.com>
2026-05-03 18:47:30 +0800

    Create src/Models/Category.php

---
+++ src/Models/Category.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+class Category extends Model
+{
+    protected static string $table = 'categories';
+    protected static bool $softDelete = true;
+
+    public static function allActive(): array
+    {
+        return static::db()->select(
+            "SELECT * FROM categories WHERE status = 'active' AND deleted_at IS NULL ORDER BY name"
+        );
+    }
+}
