6404f73670d8cd24fa44a12fcf421d91f0e2f0db
sh3ki <shekaigarcia@gmail.com>
2026-05-03 18:47:31 +0800

    Create src/Models/Color.php

---
+++ src/Models/Color.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+class Color extends Model
+{
+    protected static string $table = 'colors';
+    protected static bool $softDelete = true;
+
+    public static function allActive(): array
+    {
+        return static::db()->select(
+            "SELECT * FROM colors WHERE status = 'active' AND deleted_at IS NULL ORDER BY name"
+        );
+    }
+}
