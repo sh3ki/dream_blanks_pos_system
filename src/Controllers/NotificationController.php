d10133a77134ec19d3c145541c23ac30637524d2
sh3ki <shekaigarcia@gmail.com>
2026-05-03 18:47:26 +0800

    Create src/Controllers/NotificationController.php

---
+++ src/Controllers/NotificationController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\Request;
+use App\Core\Response;
+use App\Models\Notification;
+
+class NotificationController extends Controller
+{
+    public function index(Request $request): Response
+    {
+        [$page, $perPage] = $this->paginate($request);
+        $unreadOnly = filter_var($request->query('unread_only', false), FILTER_VALIDATE_BOOLEAN);
+        $result = Notification::forUser($this->currentUserId(), $unreadOnly, $page, $perPage);
+        return $this->success(['notifications' => $result['data'], 'pagination' => $result['pagination'], 'unread_count' => Notification::unreadCount($this->currentUserId())]);
+    }
+
+    public function markRead(Request $request): Response
+    {
+        $id = (int)$request->param('notification_id');
+        Notification::markRead($id, $this->currentUserId());
+        return $this->success(null, 'Notification marked as read');
+    }
+
+    public function destroy(Request $request): Response
+    {
+        $id = (int)$request->param('notification_id');
+        Notification::markDeleted($id, $this->currentUserId());
+        return $this->success(null, 'Notification deleted');
+    }
+}
