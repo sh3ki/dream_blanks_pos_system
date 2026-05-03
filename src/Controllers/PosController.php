861ea527a1012aab5f389bbff8c01ce36cc53be2
sh3ki <shekaigarcia@gmail.com>
2026-05-03 18:47:27 +0800

    Create src/Controllers/PosController.php

---
+++ src/Controllers/PosController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\Request;
+use App\Core\Response;
+use App\Models\Product;
+use App\Models\Category;
+use App\Services\PosService;
+
+class PosController extends Controller
+{
+    private PosService $posService;
+
+    public function __construct()
+    {
+        $this->posService = new PosService();
+    }
+
+    public function index(Request $request): Response
+    {
+        $this->requirePermission(MODULE_POS, ACTION_VIEW);
+        return $this->view('pos/index', [
+            'categories' => Category::allActive(),
+            'title'      => 'Point of Sale | Dream Blanks POS',
+        ]);
+    }
+
+    public function products(Request $request): Response
+    {
+        $this->requirePermission(MODULE_POS, ACTION_VIEW);
+        $filters  = $request->only(['search', 'category_id', 'limit']);
+        $products = Product::forPos($filters);
+        return $this->success(['products' => $products]);
+    }
+
+    public function checkout(Request $request): Response
+    {
+        $this->requirePermission(MODULE_POS, ACTION_ADD);
+        $result = $this->posService->checkout($request->all(), $this->currentUserId());
+        return $this->success($result, 'Checkout successful', 201);
+    }
+}
