f0010d299b55681dce0aa379cddf31d47a18468b
sh3ki <shekaigarcia@gmail.com>
2026-05-20 04:05:23 +0800

    Update src/Controllers/DashboardController.php

--- src/Controllers/DashboardController.php
+++ src/Controllers/DashboardController.php
@@ -18,6 +18,7 @@ class DashboardController extends Controller
 
     public function index(Request $request): Response
     {
+        $this->requirePermission(MODULE_DASHBOARD, ACTION_VIEW);
         $metrics = $this->reportService->dashboardMetrics();
         $charts  = $this->reportService->dashboardCharts('week');
         $userId  = $this->currentUserId();
