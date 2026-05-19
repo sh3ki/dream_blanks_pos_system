cdf3f62f1c05ce96e4756d1a202d01b5d45451f1
sh3ki <shekaigarcia@gmail.com>
2026-05-20 07:55:56 +0800

    Update config/routes.php

--- config/routes.php
+++ config/routes.php
@@ -27,8 +27,9 @@ $router->get('/dashboard', [\App\Controllers\DashboardController::class, 'index'
 $router->get('/pos', [\App\Controllers\PosController::class, 'index'], $auth);
 
 // Profile
-$router->get('/profile',             [\App\Controllers\UserController::class, 'profile'],            $auth);
-$router->put('/api/v1/profile',      [\App\Controllers\UserController::class, 'updateProfile'],      $auth);
+$router->get('/profile',              [\App\Controllers\UserController::class, 'profile'],            $auth);
+$router->put('/api/v1/profile',       [\App\Controllers\UserController::class, 'updateProfile'],      $auth);
+$router->get('/api/v1/profile/check-username', [\App\Controllers\UserController::class, 'checkUsername'], $auth);
 $router->post('/api/v1/profile/image',[\App\Controllers\UserController::class, 'uploadProfileImage'], $auth);
 
 // Users
