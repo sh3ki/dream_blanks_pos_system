a1b2075db06e427160ff18c7749821c67a164bf9
sh3ki <shekaigarcia@gmail.com>
2026-05-20 03:17:54 +0800

    Update src/Helpers/IconHelper.php

--- src/Helpers/IconHelper.php
+++ src/Helpers/IconHelper.php
@@ -57,6 +57,7 @@ if (!function_exists('icon')) {
             'history'        => '<circle cx="12" cy="12" r="10"/><polyline points="12 6 12 12 16 14"/>',
             'payment'        => '<rect x="1" y="4" width="22" height="16" rx="2" ry="2"/><line x1="1" y1="10" x2="23" y2="10"/>',
             'restock'        => '<polyline points="23 4 23 10 17 10"/><polyline points="1 20 1 14 7 14"/><path d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"/>',
+            'audit'          => '<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="16" y1="13" x2="8" y2="13"/><line x1="16" y1="17" x2="8" y2="17"/><polyline points="10 9 9 9 8 9"/>',
         ];
 
         $path = $icons[$name] ?? $icons['info'];
