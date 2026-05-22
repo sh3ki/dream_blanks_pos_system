c7a1e702a338963429f073e5a3ce619f03a2cf64
sh3ki <shekaigarcia@gmail.com>
2026-05-23 05:42:04 +0800

    Update src/Helpers/IconHelper.php

--- src/Helpers/IconHelper.php
+++ src/Helpers/IconHelper.php
@@ -61,6 +61,8 @@ if (!function_exists('icon')) {
             'payment'        => '<rect x="1" y="4" width="22" height="16" rx="2" ry="2"/><line x1="1" y1="10" x2="23" y2="10"/>',
             'restock'        => '<polyline points="23 4 23 10 17 10"/><polyline points="1 20 1 14 7 14"/><path d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"/>',
             'audit'          => '<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="16" y1="13" x2="8" y2="13"/><line x1="16" y1="17" x2="8" y2="17"/><polyline points="10 9 9 9 8 9"/>',
+            'forward'        => '<polyline points="13 17 18 12 13 7"/><path d="M6 12h12"/>',
+            'lineup'         => '<line x1="8" y1="6" x2="21" y2="6"/><line x1="8" y1="12" x2="21" y2="12"/><line x1="8" y1="18" x2="21" y2="18"/><line x1="3" y1="6" x2="3.01" y2="6"/><line x1="3" y1="12" x2="3.01" y2="12"/><line x1="3" y1="18" x2="3.01" y2="18"/>',
         ];
 
         $path = $icons[$name] ?? $icons['info'];
