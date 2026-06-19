c882459c3a1c53f147bff51eb41038e76656fd2d
sh3ki <shekaigarcia@gmail.com>
2026-06-19 10:35:57 +0800

    Update src/Models/Category.php

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -10,7 +10,7 @@ class Category extends Model
     public static function allActive(): array
     {
         return static::db()->select(
-            "SELECT * FROM categories WHERE status = 'active' AND deleted_at IS NULL ORDER BY name"
+            "SELECT * FROM categories WHERE status = 'active' AND deleted_at IS NULL ORDER BY `order` ASC, name ASC"
         );
     }
 }
